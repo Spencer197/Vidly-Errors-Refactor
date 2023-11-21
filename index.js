5f7126e4f33d14ed0db24f73bcbe8c16d3e3063c
Spencer197 <spncr@protonmail.com>
2023-11-21 21:47:42 +0900

    Extract config logic & add config.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 require('./startup/logging');//Loads startup/logging first to log any startup error.
 require('./startup/routes')(app);//Loads startup/routes module, returns function to call app obj. 
 require('./startup/db')();//Load startup/db and call the returned function ().
-require('./startup/config')();
+require('./startup/config')();//Load startup/config and call returned function ().
 require('./startup/validation')();
 
 const port = process.env.PORT || 3000;
--- startup/config.js
+++ startup/config.js
@@ -1,7 +1,8 @@
-const config = require('config');
+const config = require('config');//Cut and pasted from index.js
 
 module.exports = function() {
-  if (!config.get('jwtPrivateKey')) {
-    throw new Error('FATAL ERROR: jwtPrivateKey is not defined.');
+  if (!config.get('jwtPrivateKey')) {//Cut and pasted from index.js
+    throw new Error('FATAL ERROR: jwtPrivateKey is not defined.');//Throw exception, cache it, log it, & terminate process
   }
 }
+//Best Practice: Throw Error object instead of a string so that stacktrace is available.
