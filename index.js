2e3b3b93fbd15000aa6c2f1ee263fe288317e4c8
Spencer197 <spncr@protonmail.com>
2023-11-21 22:04:05 +0900

    Extract validation logic and add validation.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const winston = require('winston');
+const winston = require('winston');//Load winston for logging port connection below.
 const express = require('express');
 const app = express();
 
@@ -6,7 +6,7 @@ require('./startup/logging');//Loads startup/logging first to log any startup er
 require('./startup/routes')(app);//Loads startup/routes module, returns function to call app obj. 
 require('./startup/db')();//Load startup/db and call the returned function ().
 require('./startup/config')();//Load startup/config and call returned function ().
-require('./startup/validation')();
+require('./startup/validation')();//Load startup/validation and call exported function ().
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => winston.info(`Listening on port ${port}...`));
--- startup/validation.js
+++ startup/validation.js
@@ -1,5 +1,5 @@
-const Joi = require('joi');
+const Joi = require('joi');//From index.js
 
 module.exports = function() {
-  Joi.objectId = require('joi-objectid')(Joi);
+  Joi.objectId = require('joi-objectid')(Joi);//From index.js
 }
